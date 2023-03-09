d35015aed93b83237020661056826081e545102e
孙启元10312862 <sun.qiyuan@zte.com.cn>
2023-03-09 13:38:31 +0800

    #2 添加左侧导航图片以及树 viewContainer 和treeview

--- dev_readme.md
+++ dev_readme.md
@@ -1,2 +1,16 @@
 # 调试
 入口 extension.ts , 按f5
+# 最左侧的图标是  activitybar， 相当于导航栏
+
+https://code.visualstudio.com/api/references/contribution-points#contributes.viewsContainers
+
+# 左侧面板
+树状 view 的demo
+> viewsContainers 最左侧导航栏:  1. 向已有 view (exporer\debug\scm\test)  2. 自定义 view
+> view 左侧panel比如文件树
+
+
+ui https://code.visualstudio.com/api/ux-guidelines/views#links
+api https://code.visualstudio.com/api/references/contribution-points#contributes.views
+demo https://github.com/microsoft/vscode-extension-samples/tree/main/tree-view-sample
+
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { ExplorDataProvider } from './snippets--explor';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -20,6 +21,8 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+	// 注册 item
+	vscode.window.registerTreeDataProvider('snippets--explor', new ExplorDataProvider());
 }
 
 // This method is called when your extension is deactivated
